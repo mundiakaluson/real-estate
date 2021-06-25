ade04e562681980d1caa2a9b1a8ce668c1230793
mundiakaluson <mundiakaluson@gmail.com>
2021-06-25 16:54:03 +0300

    do change

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = '+kad@m2sawr_rcgw^ync*92+#pc8&8v(6hufcz#ig&r&9-r@^9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://buyrentglobal.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'https://buyrentglobal.herokuapp.com', '147.182.135.197']
 
 
 
