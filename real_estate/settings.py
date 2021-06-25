4acd2d80129da372a9f4f17bc5dfe8dbfe0fb2c9
mundiakaluson <mundiakaluson@gmail.com>
2021-06-25 19:49:57 +0300

    again

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -138,14 +138,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(PROJECT_DIR, 'media')
 
-MEDIA_URL = '/media/'
-
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
-STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(PROJECT_DIR, 'staticfiles'),
